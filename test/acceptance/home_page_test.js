a4d874d76707a33a691fb26fd170307ceb82fa1f
jwicksnin <jwicksnin@gmail.com>
2014-03-24 15:56:13 -0700

    work casper testing

--- test/acceptance/home_page_test.js
+++ test/acceptance/home_page_test.js
@@ -1,15 +1,15 @@
 'use strict';
 /*global casper*/
 
-casper.test.begin('home page', 3, function suite(test) {
+casper.test.begin('home page', 2, function suite(test) {
 
   casper.start('http://localhost:3000/', function() {
     test.assertHttpStatus(200);
   });
 
-  // casper.then(function(){
-  //   test.assertTitle('Jessica Portfolio Page | Home', 'title is Jessica Portfolio Page');
-  // });
+  casper.then(function(){
+    test.assertTitle('Jessica Portfolio Page', 'title is Jessica Portfolio Page');
+  });
 
   // casper.then(function() {
   //   test.assertSelectorHasText('h1.largeHeader','OurAgendaApp');
